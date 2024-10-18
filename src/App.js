91ef7d333cf88c2ac13fc13ebda57bf85d14e84a
unknown <bankarpm2001@gmail.com>
2024-10-18 17:48:46 +0530

    Monthly report and memberpage css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate,Link } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
 import MemberRegistration from './components/Member-Registration';
-// import Members from './components/MembersPage'
-import MembersPage from './components/MembersPage';
+import MembersPage from './components/MembersPage'
+import Download_Monthly_Report from './components/Download_Monthly_Report'
 
 
 
@@ -15,13 +15,14 @@ function App() {
       
       <Routes>2
         {/* Redirect root path to the login page */}
-        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Navigate to="/login" />} />
         
         <Route exact path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home/>} />
         <Route path="/member-registration" element={<MemberRegistration/>}/>
-        <Route path="/members" element={<MembersPage/>}/>
+        <Route path="/Download_Monthly_Report" element={<Download_Monthly_Report/>}/>
+        <Route path="/MembersPage" element={<MembersPage/>}/>
 
       </Routes>
     </div>
